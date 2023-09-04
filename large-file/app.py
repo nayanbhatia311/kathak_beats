beb63c8e203c3d8f45c8276b00e941104632b55d
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-09-03 20:13:33 -0700

    modify app.py to work on uploaded video

--- large-file/app.py
+++ large-file/app.py
@@ -1,7 +1,11 @@
 import os
-from model import detect_peaks_in_song
+import subprocess as sp
+import librosa
+import serial
 from flask import Flask, render_template, request
 from flask_dropzone import Dropzone
+
+# Add your own separate() and find_files() functions here, or import them if they are in a separate module.
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -9,13 +13,11 @@ app = Flask(__name__)
 
 app.config.update(
     UPLOADED_PATH=os.path.join(basedir, 'uploads'),
-    # Flask-Dropzone config:
     DROPZONE_MAX_FILE_SIZE=1024,
     DROPZONE_TIMEOUT=5 * 60 * 1000
 )
 
 dropzone = Dropzone(app)
-
 
 @app.route('/', methods=['POST', 'GET'])
 def upload():
@@ -23,10 +25,35 @@ def upload():
         f = request.files.get('file')
         path = os.path.join(app.config['UPLOADED_PATH'], f.filename)
         f.save(path)
-        array_of_peaks = detect_peaks_in_song(path)
-        return array_of_peaks
-    return render_template('index.html')
+        
+        # Call your separate function to separate vocals
+        separate(inp=path, outp=os.path.join(basedir, 'output'))
+        
+        # Load the separated vocal track (adjust path as needed)
+        audio_file_path = os.path.join(basedir, f'output/htdemucs/{f.filename}/vocals.mp3')
+        audio, sr = librosa.load(audio_file_path)
+        
+        # Onset detection
+        onset_env = librosa.onset.onset_strength(y=audio, sr=sr)
+        onset_frames = librosa.onset.onset_detect(onset_envelope=onset_env, sr=sr)
+        onset_times = librosa.frames_to_time(onset_frames, sr=sr)
+        
+        # Open COM port 3
+        try:
+            ser = serial.Serial('COM3', 9600, timeout=1)
+        except Exception as e:
+            return f"Failed to open COM port: {e}"
 
+        # Write onset timestamps to COM port 3
+        for time in onset_times:
+            ser.write(f"{time}\n".encode())
+        
+        # Close the COM port
+        ser.close()
+
+        return str(onset_times)
+        
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
