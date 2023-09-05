de85cf65ed059b93696964d4953be8cfa1242cfb
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-09-04 19:28:33 -0700

    add max file size and uploads folder support

--- large-file/app.py
+++ large-file/app.py
@@ -5,54 +5,55 @@ import serial
 from flask import Flask, render_template, request
 from flask_dropzone import Dropzone
 
-# Add your own separate() and find_files() functions here, or import them if they are in a separate module.
-
+# Initialize your Flask application and Dropzone
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.config.update(
     UPLOADED_PATH=os.path.join(basedir, 'uploads'),
-    DROPZONE_MAX_FILE_SIZE=1024,
+    DROPZONE_MAX_FILE_SIZE=999999999,
     DROPZONE_TIMEOUT=5 * 60 * 1000
 )
 
 dropzone = Dropzone(app)
 
+if not os.path.exists(os.path.join(basedir, 'uploads')):
+    os.makedirs(os.path.join(basedir, 'uploads'))
+
 @app.route('/', methods=['POST', 'GET'])
 def upload():
     if request.method == 'POST':
         f = request.files.get('file')
+        
+        print(f"Received file named {f.filename}")
+        
         path = os.path.join(app.config['UPLOADED_PATH'], f.filename)
-        f.save(path)
-        
-        # Call your separate function to separate vocals
-        separate(inp=path, outp=os.path.join(basedir, 'output'))
-        
+
+        print(f"Saving file to {path}")
+
+        try:
+            f.save(path)
+        except Exception as e:
+            return f"Could not save file: {e}"
+
+
         # Load the separated vocal track (adjust path as needed)
         audio_file_path = os.path.join(basedir, f'output/htdemucs/{f.filename}/vocals.mp3')
-        audio, sr = librosa.load(audio_file_path)
         
-        # Onset detection
-        onset_env = librosa.onset.onset_strength(y=audio, sr=sr)
-        onset_frames = librosa.onset.onset_detect(onset_envelope=onset_env, sr=sr)
-        onset_times = librosa.frames_to_time(onset_frames, sr=sr)
-        
-        # Open COM port 3
+        # Debugging: Print the audio file path
+        print(f"Looking for file at {audio_file_path}")
+
         try:
-            ser = serial.Serial('COM3', 9600, timeout=1)
+            audio, sr = librosa.load(audio_file_path)
         except Exception as e:
-            return f"Failed to open COM port: {e}"
+            return f"Could not load audio file: {e}"
 
-        # Write onset timestamps to COM port 3
-        for time in onset_times:
-            ser.write(f"{time}\n".encode())
-        
-        # Close the COM port
-        ser.close()
 
-        return str(onset_times)
-        
+        # COM port operations here (assuming you have one)
+
+        return str("Replace this with your onset times or other return values")
+
     return render_template('index.html')
 
 if __name__ == '__main__':
