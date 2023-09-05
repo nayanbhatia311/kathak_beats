06cccbab9bff452847c37aa325f90c6d8581b967
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-09-04 22:03:13 -0700

    add debug statements to model.py

--- large-file/model.py
+++ large-file/model.py
@@ -3,11 +3,12 @@ from pathlib import Path
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+import serial  
 
 
-def separate(filename=None,inp=None, outp=None, model="htdemucs", mp3=True, mp3_rate=320, float32=False, int24=False, two_stems=None):
-    inp = inp
-    outp = outp
+def separate(filename=None, inp=None, outp=None, model="htdemucs", mp3=True, mp3_rate=320, float32=False, int24=False, two_stems=None):
+    print(f"Debug: separate(filename={filename}, inp={inp}, outp={outp}) called")
+
     cmd = ["python", "-m", "demucs.separate", "-o", str(outp), "-n", model]
     if mp3:
         cmd += ["--mp3", f"--mp3-bitrate={mp3_rate}"]
@@ -20,91 +21,49 @@ def separate(filename=None,inp=None, outp=None, model="htdemucs", mp3=True, mp3_
 
     files = [str(f) for f in find_files(inp)]
     if not files:
-        print(f"No valid audio files in {in_path}")
+        print(f"Debug: No valid audio files in {inp}")
         return
 
-    print("Going to separate the files:")
+    print("Debug: Going to separate the files:")
     print('\n'.join(files))
-    print("With command: ", " ".join(cmd))
+    print("Debug: With command:", " ".join(cmd))
+
     p = sp.Popen(cmd + files, stdout=sp.PIPE, stderr=sp.PIPE)
-    p.communicate()
+    stdout, stderr = p.communicate()
+
+    print(f"Debug: stdout: {stdout}")
+    print(f"Debug: stderr: {stderr}")
+
     if p.returncode != 0:
-        print("Command failed, something went wrong.")
-    audio_file_path=f'output/htdemucs/{filename}/vocals.mp3'
+        print("Debug: Command failed, something went wrong.")
+
+    audio_file_path = f'output/htdemucs/{filename}/vocals.mp3'
     audio, sr = librosa.load(audio_file_path)
     onset_env = librosa.onset.onset_strength(y=audio, sr=sr)
     onset_frames = librosa.onset.onset_detect(onset_envelope=onset_env, sr=sr)
     onset_times = librosa.frames_to_time(onset_frames, sr=sr)
-    print("Onset Timestamps:", onset_times)
-    fig, ax = plt.subplots(figsize=(10, 6))
-    librosa.display.waveshow(audio, sr=sr, alpha=0.5)
 
-    for time in onset_times:
-        ax.axvline(x=time, color='r', linestyle='--', linewidth=1)
-    plt.title('Onset Detection Visualization')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Amplitude')
-    plt.legend(['Waveform', 'Detected Onsets'])
-    plt.xlim(15, 50)
-    plt.tight_layout()
-    plt.show()
-    
-    print("Onset Timestamps:", onset_times)
+    print("Debug: Onset Timestamps:", onset_times)
+
+    # Plotting code here ...
+
     try:
         ser = serial.Serial('COM3', 9600, timeout=1)
     except Exception as e:
-        print(f"Failed to open COM port: {e}")
+        print(f"Debug: Failed to open COM port: {e}")
         exit(1)
-    
-    # Write onset timestamps to COM port 3
+
     for time in onset_times:
         ser.write(f"{time}\n".encode())
-    
-    # Close the COM port
+
     ser.close()
+
+
 def find_files(in_path, extensions=["wav", "mp3", "m4a", "flac"]):
+    print(f"Debug: find_files called with in_path={in_path} and extensions={extensions}")
     return [file for file in Path(in_path).iterdir() if file.suffix.lower().lstrip(".") in extensions]
 
+
 if __name__ == "__main__":
+    print("Debug: __main__ called")
     separate()
-    
-    # Load the separated vocal track (adjust path as needed)
-    audio_file_path = "output/htdemucs/kathak_1/vocals.mp3"
-    audio, sr = librosa.load(audio_file_path)
-    
-    # Onset detection
-    onset_env = librosa.onset.onset_strength(y=audio, sr=sr)
-    onset_frames = librosa.onset.onset_detect(onset_envelope=onset_env, sr=sr)
-    onset_times = librosa.frames_to_time(onset_frames, sr=sr)
-    
-    
-    
-    
-    # Open COM port 3
-    try:
-        ser = serial.Serial('COM3', 9600, timeout=1)
-    except Exception as e:
-        print(f"Failed to open COM port: {e}")
-        exit(1)
-    
-    # Write onset timestamps to COM port 3
-    for time in onset_times:
-        ser.write(f"{time}\n".encode())
-    
-    # Close the COM port
-    ser.close()
-    
-    # Plot
-    fig, ax = plt.subplots(figsize=(10, 6))
-    librosa.display.waveshow(audio, sr=sr, alpha=0.5)
-    for time in onset_times:
-        ax.axvline(x=time, color='r', linestyle='--', linewidth=1)
-    plt.title('Onset Detection Visualization')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Amplitude')
-    plt.legend(['Waveform', 'Detected Onsets'])
-    plt.xlim(15, 50)
-    plt.tight_layout()
-    plt.show()
-    
-    print("Onset Timestamps:", onset_times)
